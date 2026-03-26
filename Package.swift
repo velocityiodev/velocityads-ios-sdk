e04a3910246108ad0909f58c3082036aacb5826a
Roi Eshel <kendstar@gmail.com>
2026-03-26 18:24:29 +0200

    Release 0.1.1

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .binaryTarget(
             name: "VelocityAdsSDK",
             url: "https://github.com/velocityiodev/velocityads-ios-sdk/releases/download/0.1.1/VelocityAdsSDK-0.1.1.zip",
-            checksum: "5289d0bc00e987543a802d20c2a2e49c5ab2a770adeddd68ade97e16d6487101"
+            checksum: "9a4a2afcfa34e1c9b809d22a56e64dce5942612071a62eb0efeba86d399f7a87"
         ),
     ]
 )
